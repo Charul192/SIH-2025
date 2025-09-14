7fd79278f013d835944984ced3f69f0f8554bf42
Harman0007-hacker <harmansingla454@gmail.com>
2025-09-14 16:47:30 +0530

    env files created

--- Frontend/src/maprender.tsx
+++ Frontend/src/maprender.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import {createRoot} from "react-dom/client"
 import {APIProvider, Map} from "@vis.gl/react-google-maps"
+import dotenv from "dotenv"
+dotenv.config();
 const App = () => {
     return (
-        < APIProvider apiKey={"AIzaSyARSqYspchcCQGDRl1izB0_GaqQ6A2Yz6w"}>
+        <APIProvider apiKey={process.env.GOOGLE_API_KEY ?? ""}>
             <Map 
                 defaultZoom={13}
                 defaultCenter={ { lat: -33.860664, lng: 151.208138 } } >
