849322b4f92bdc18a6ad7b48c86898956cdf5fdd
Harman0007-hacker <harmansingla454@gmail.com>
2025-09-14 12:04:18 +0530

    manipulating the backend

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,9 @@ const app= express();
 const PORT=8000
 app.use(express.json());
 console.log("Hello")
+app.get('/', (req, res)=>{
+    res.sendFile('./index.html')
+})
 app.listen(PORT, () => {
     console.log(`Server started at PORT ${PORT}`);
 });
--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav className="flex items-center justify-between py-6">
         <div className="fixed flex flex-wrap justify-center top-8 inset-x-0 px-2">
           <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-slate-800 rounded-2xl px-3 py-2 '>
-            <button onClick={() => setColor("White")} className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg' style={{ backgroundColor: "black" }}>Home</button>
-            <button onClick={() => setColor("beige")} className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg' style={{ backgroundColor: "black" }}>Track</button>
-            <button onClick={() => setColor("olive")} className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg' style={{ backgroundColor: "black" }}>Plan</button>
-            <button onClick={() => setColor("olive")} className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg' style={{ backgroundColor: "black" }}>Schedule</button>
-            <button onClick={() => setColor("grey")} className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg' style={{ backgroundColor: "black" }}>About</button>
+             <a className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg cursor-pointer' style={{ backgroundColor: "black" }} href="#">Home</a>
+             <a className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg cursor-pointer' style={{ backgroundColor: "black" }} href="../../tracker.html">Track</a>
+             <a className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg cursor-pointer' style={{ backgroundColor: "black" }} href="">Plan</a>
+             <a className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg cursor-pointer' style={{ backgroundColor: "black" }} href="">Schedule</a>
+             <a className='outline-none px-4 py-1 rounded-2xl text-white shadow-lg cursor-pointer' style={{ backgroundColor: "black" }} href="">About</a>
           </div>
         </div>
     </nav>
