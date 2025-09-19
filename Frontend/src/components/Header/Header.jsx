d1f2a05128f04c0780a68c69ab73a77c1f3fa84a
Aniket Kedia <kediaaniket27@gmail.com>
2025-09-20 02:00:13 +0530

    modified about page

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"; // 1. useState ko import karein
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../Logo/Logo";
+import Logo from "../logo/logo";
 
 export default function Header() {
   // 2. Menu ki state (khula/band) ko manage karne ke liye
