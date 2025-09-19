30db79a1b8971e348817bf8cce85faeaa3d38c5c
AashwatJain <aashwatjain2005@gmail.com>
2025-09-19 23:47:26 +0530

    ghjasg

--- Frontend/src/components/Logo/Logo.jsx
+++ Frontend/src/components/Logo/Logo.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Make sure to replace './path-to-your-logo.png' with the actual path to your image file.
-import logoImage from 'C:/Users/Aniket/OneDrive/Desktop/Coding/SIH-2025/Frontend/public/WhatsApp Image 2025-09-19 at 15.29.36_e16afdfa.jpg'
+import logoImage from '../../../public/logo.jpg'
 function Logo() {
   return (
     <div className="flex items-center space-x-2">
