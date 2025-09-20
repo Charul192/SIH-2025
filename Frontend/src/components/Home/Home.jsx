0bbd8bc25cdeaee421fd84e3bb59398db77cee5e
Aniket Kedia <kediaaniket27@gmail.com>
2025-09-20 21:05:01 +0530

    FAQs

--- Frontend/src/components/Home/Home.jsx
+++ Frontend/src/components/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AppContext } from "../../context/AppContext";
+import FAQ from "../FAQs/faqs";
 
 // Helper components
 const CheckIcon = ({ className }) => (
@@ -127,6 +128,7 @@ export default function Home() {
                   </div>
               ))}
             </div>
+            <FAQ/>
           </div>
         </div>
       </main>
